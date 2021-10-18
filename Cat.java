a4ce0de949145ac6e97b5c7116457a7747c7346b
RusOsh <90300562+RusOsh@users.noreply.github.com>
2021-10-18 14:42:50 +0600

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Cat extends Animal{
+
+    private int lives;
+
+
+    public Cat(String name, int lives) {
+        super(name);
+        this.lives = lives;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Name: " + getName() +
+                "\n# of lives: " + lives);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Main {
+
+    public static Dog dog = new Dog("Simba", new String[]{"Sit", "Attack"});
+    public static Cat cat = new Cat("Vasjka", 9);
+    public static Snake snake = new Snake("Kobra", "Kobra's bite is hurts");
+    public static Animal[] animals = {dog, cat, snake};
+
+
+
+    public static void main(String[] args) {
+        for (Animal animal : animals) {
+            animal.print();
+        }
+    }
+
+    public static Animal createObject() {
+        return createObject(animals.toString());
+    }
+
+    public static Animal createObject(String className) {
+        switch (className) {
+            case "Dog" -> {
+                System.out.println("Case Dog has been revoked:");
+                return dog;
+            }
+            case "Cat" -> {
+                System.out.println("Class Cat has been revoked:");
+                return cat;
+            }
+            case "Snake" -> {
+                System.out.println("Class Snake has been revoked:");
+                return snake;
+            }
+            default -> {
+                return dog;
+            }
+        }
+    }
+}
